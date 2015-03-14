2e1b1ceb2ca744ac2238d031f5059619af7e891e
Jimmy Schementi <jschementi@gmail.com>
2015-03-14 10:08:40 -0400

    whitespace

--- client/js/cards.js
+++ client/js/cards.js
@@ -2,13 +2,13 @@
 
 var Card = Backbone.Model.extend({
     initialize: function () {
-        this.listenTo(this, 'add', this.updateFlipForCollection);  
+        this.listenTo(this, 'add', this.updateFlipForCollection);
     },
-    
+
     flip: function () {
         this.set('faceup', !this.get('faceup'));
     },
-    
+
     updateFlipForCollection: function () {
         if (this.collection && this.collection.faceup !== null) {
             this.set('faceup', this.collection.faceup);
@@ -18,16 +18,16 @@ var Card = Backbone.Model.extend({
 
 var CardList = Backbone.Collection.extend({
     model: Card,
-    
+
     initialize: function (models, options) {
         this.faceup = options && typeof options.faceup === 'boolean' ? options.faceup : null;
     },
-    
+
     shuffle: function () {
         this.reset(_.shuffle(this.models));
         return this;
     },
-    
+
     reorder: function(newIndex, originalIndex) {
         var temp = this.at(originalIndex);
         this.remove(temp, {silent: true});
@@ -82,7 +82,7 @@ var Game = Backbone.Model.extend({
         }
         Backbone.Model.apply(this, arguments);
     },
-    
+
     initialize: function () {
         this.set({
             deck: createDeck(),
@@ -92,7 +92,7 @@ var Game = Backbone.Model.extend({
             }.bind(this))),
         });
     },
-    
+
     deal: function () {
         var deck = this.get('deck');
         var players = this.get('players');
@@ -105,7 +105,7 @@ var Game = Backbone.Model.extend({
         deck.popTo(discardPile);
         this.nextTurn();
     },
-    
+
     nextTurn: function () {
         var index = this.get('currentPlayerIndex');
         if (typeof index !== 'number') {
@@ -115,20 +115,20 @@ var Game = Backbone.Model.extend({
         this.set('currentPlayerIndex', index);
         this.set('currentPlayer', this.get('players').at(this.get('currentPlayerIndex')));
         this.set('hasPickedCard', false);
-        
+
         if (this.get('currentPlayer').get('bot')) {
             this.doBotTurn();
         }
     },
-    
+
     shouldPickCard: function () {
         return !this.get('hasPickedCard');
     },
-    
+
     shouldDiscard: function () {
         return this.get('hasPickedCard');
     },
-    
+
     pickFromStock: function () {
         if (!this.shouldPickCard()) {
             return;
@@ -147,7 +147,7 @@ var Game = Backbone.Model.extend({
             this.get('discardPile').push(this.get('deck').pop());
         }
     },
-    
+
     pickFromDiscard: function () {
         if (!this.shouldPickCard()) {
             return;
@@ -163,7 +163,7 @@ var Game = Backbone.Model.extend({
 
         this.set('hasPickedCard', true);
     },
-    
+
     tryingToDiscard: function (cardIndex) {
         if (!this.shouldDiscard()) {
             return;
@@ -198,9 +198,9 @@ var CardView = Backbone.View.extend({
         this.listenTo(this.model, 'change:faceup', this.showSide);
         this.listenTo(this.model, 'remove', this.remove);
     },
-    
+
     className: 'card-container',
-    
+
     template: _.template(
         '<div class="card">' +
         '    <div class="front face suit-<%= card.suit %>">' +
@@ -210,7 +210,7 @@ var CardView = Backbone.View.extend({
         '    <div class="back face">' +
         '    </div>' +
         '</div>', {variable: 'card'}),
-    
+
     getValue: function () {
         var value = this.model.get('value');
         if (typeof value === 'string') {
@@ -218,7 +218,7 @@ var CardView = Backbone.View.extend({
         }
         return value + '';
     },
-    
+
     getSuit: function () {
         switch (this.model.get('suit').toLowerCase()[0]) {
             case 's': return "♠";
@@ -227,7 +227,7 @@ var CardView = Backbone.View.extend({
             case 'c': return "♣";
         }
     },
-    
+
     render: function () {
         this.$el.html(this.template({
             suit: this.getSuit(),
@@ -236,7 +236,7 @@ var CardView = Backbone.View.extend({
         this.showSide(this.model, this.model.get('faceup'));
         return this;
     },
-    
+
     showSide: function (model, faceup) {
         this.$el.toggleClass('flip', !faceup);
     }
@@ -249,21 +249,21 @@ var ListView = Backbone.View.extend({
         }
         Backbone.View.apply(this, arguments);
     },
-    
+
     initialize: function () {
         this.listenTo(this.collection, 'add', this.addOne);
         this.listenTo(this.collection, 'reset', this.addAll);
     },
-    
+
     render: function () {
         this.addAll();
         return this;
     },
-    
+
     addOne: function (model) {
         this.$el.append(new this.ItemView({model: model}).render().el);
     },
-    
+
     addAll: function () {
         this.$el.empty();
         for (var i = 0; i < this.collection.length; i++) {
@@ -274,14 +274,14 @@ var ListView = Backbone.View.extend({
 
 var CardListView = ListView.extend({
     className: 'card-list',
-    
+
     ItemView: CardView,
-    
+
     initialize: function (options) {
         this.reorder = !!(options || {}).reorder;
         ListView.prototype.initialize.apply(this, arguments);
     },
-    
+
     render: function () {
         var result = ListView.prototype.render.apply(this, arguments);
         if (this.reorder) {
@@ -328,7 +328,7 @@ var GameView = Backbone.View.extend({
         'click #discard': 'pickFromDiscard',
         'click .handcontainer.you .card-container': 'tryingToDiscard'
     },
-    
+
     render: function () {
         var deckEl = $('<div id="deck"/>').appendTo(this.el);
         new CardListView({collection: this.model.get('deck'), className: 'stack', id: 'stock'}).render().$el.appendTo(deckEl);
@@ -336,26 +336,26 @@ var GameView = Backbone.View.extend({
         new PlayersView({collection: this.model.get('players')}).render().$el.appendTo(this.el);
         return this;
     },
-    
+
     shouldAllowUI: function () {
         return !this.model.get('currentPlayer').get('bot');
     },
-    
+
     pickFromStock: function () {
         if (!this.shouldAllowUI()) {
             return;
         }
-        
+
         this.model.pickFromStock();
     },
-    
+
     pickFromDiscard: function () {
         if (!this.shouldAllowUI()) {
             return;
         }
         this.model.pickFromDiscard();
     },
-    
+
     tryingToDiscard: function (e) {
         if (!this.shouldAllowUI()) {
             return;
